cd816799afc399ceb27b8a43bedd03eaeeb29025
YiJoonYong <yijy001@gmail.com>
2021-09-14 15:19:59 +0900

    210914 image widget

---
+++ App/Widget/image_demo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ImageDemo());
+
+class ImageDemo extends StatelessWidget {
+  static const String _title = "Image demo";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: _title,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Text(_title)),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/moon.jpg'),
+              Image.asset('assets/moon2.jpg'),
+            ],
+          ),
+        ));
+  }
+}
