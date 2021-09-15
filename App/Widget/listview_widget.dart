d4a5d407d552ab18754a51dc8b10d3889d7fd0c0
YiJoonYong <yijy001@gmail.com>
2021-09-15 16:33:54 +0900

    210915 dart listview

---
+++ App/Widget/listview_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ListViewStaticDemo());
+
+class ListViewStaticDemo extends StatelessWidget {
+  final String _title = "Static ListView Widget Demo";
+  final List<String> _data = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune',
+    'Pluto',
+  ];
+
+  Widget _buildStaticListView() {
+    return ListView.builder(
+      itemCount: _data.length,
+      itemBuilder: (BuildContext _context, int i) {
+        return ListTile(
+          title: Text(_data[i], style: TextStyle(fontSize: 23)),
+          trailing: Icon(
+            Icons.favorite_border,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: _title,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Text(_title)),
+          body: _buildStaticListView(),
+        ));
+  }
+}
