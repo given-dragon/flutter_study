0c40751c00b1fdec563486b37a62784c21da23c1
YiJoonYong <yijy001@gmail.com>
2021-09-29 16:30:43 +0900

    210929 flutter_future

---
+++ App/pj/flutter_future_ex/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future_ex',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String result = 'no data found';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Future Test'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                  onPressed: () {
+                    futureTest();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Future test',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                result,
+                style: TextStyle(fontSize: 20.0, color: Colors.redAccent),
+              ),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+              ),
+              FutureBuilder(
+                  future: myFuture(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Text(
+                        snapshot.data.toString(),
+                        style: TextStyle(fontSize: 20.0, color: Colors.blue),
+                      );
+                    }
+                    return CircularProgressIndicator();
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> futureTest() async {
+    await Future.delayed(Duration(seconds: 3)).then((value) {
+      print('Here cones second');
+      setState(() {
+        result = 'The data is fetched';
+        print(result);
+        print('Here cones third');
+      });
+    });
+    print('Here comes first');
+    print('Here is the last one');
+  }
+}
+
+Future<String> myFuture() async {
+  await Future.delayed(Duration(seconds: 2));
+  return 'another Future completed';
+}
